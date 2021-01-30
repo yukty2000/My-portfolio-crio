38606dc57eba615b00e35dff77fbe32134cff7b4
yukty2000 <55875848+yukty2000@users.noreply.github.com>
2021-01-30 13:03:06 +0530

    Add files via upload

---
+++ Frontend/src/data/config.js
@@ -0,0 +1,13 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Yukty Khandelia",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Yukty Khandelia and I'm a CSE 3rd Year Undergrad!",
+  githubUrl: "https://github.com/yukty2000",
+  linkedinUrl: "https://www.linkedin.com/in/yukty-khandelia-5a19951a3/",
+  cvLink: "https://drive.google.com/file/d/1Q1EnZ7UpwVyXWL7B9Jy4vgTJ3-EjdSUC/view?usp=sharing",
+  cflink:"https://codeforces.com/profile/yukty2000",
+  leetlink: "https://leetcode.com/Yukty/",
+  cheflink: "https://www.codechef.com/users/magdalene"
+};
